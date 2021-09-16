8f272630f6a088c07ca77cc28d1254c5de3f811b
isMimino <lokaloka5489318@gmail.com>
2021-09-17 02:49:39 +0300

    db:migrate

--- db/migrate/20210916234457_create_sessions.rb
+++ db/migrate/20210916234457_create_sessions.rb
@@ -1,8 +1,11 @@
 class CreateSessions < ActiveRecord::Migration[6.1]
   def change
     create_table :sessions do |t|
-
+      t.string :session_id, null: false
+      t.text :data
       t.timestamps
-    end
+  end
+  add_index :sessions, :session_id
+  add_index :sessions, :updated_at
   end
 end
